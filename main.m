183d8008b76c5ca8b3eac051432d7b0a0d27c24a
Cao-Cuong Do <88456061+cuongdc03@users.noreply.github.com>
2023-11-27 00:23:36 +0700

    Final

--- main.m
+++ main.m
@@ -3,35 +3,35 @@ close; clear; clc;
 NguyenAm = {'a','e','i','o','u'};
 TenFolder = {'01MDA','02FVA','03MAB','04MHB','05MVB','06FTB','07FTC','08MLD','09MPD','10MSD','11MVD','12FTD','14FHH','15MMH','16FTH','17MTH','18MNK','19MXK','20MVK','21MTL','22MHL'};
 % goi ham va truyen thong so vao cho cac bien
-MaTranNhamLan=zeros(5,5);                    
+MaTranNhamLan=zeros(6,6);                    
 [VectorDacTrung] = TimVectorDacTrung(); 
-SoPhanTuVector_fft = 2048;         % N_FFT:128, 256, 512, 2048,4096,...       
+SoPhanTuVector_fft = 1024;         % N_FFT:128, 256, 512, 2048,4096,...       
 DoDaiKhung = 0.03;                               
 DoDichKhung = 0.01;                            
-
-% duyet lan luot tung nguyen am cua tung folder kiem thu
+NguongXacDinhcuaKhoangLang =0.01;
+% duyet lan luot tung nguyen am cua tung folder kiem thul
 for i = 1 : length(TenFolder)
     disp(strcat('File : ',TenFolder(i)))
     for j = 1 : length(NguyenAm)
         path = char(strcat('NguyenAmKiemThu-16k/',TenFolder(i),'/',NguyenAm(j),'.wav'));
         [x, fs] = audioread(path);                  
         x = x / max(abs(x));        % chuan hoa tin hieu ve day -1 : 1
-        [x] = CatKhoangLang(x,fs,0.01);           
+        [x] = CatKhoangLang(x,fs,NguongXacDinhcuaKhoangLang);           
         [x] = LayDoanOnDinh(x);                    
         SoLuongKhung = floor((size(x,1) - DoDaiKhung*fs)/(DoDichKhung*fs)) + 1;% tinh so luong khung
-        Total = 0;                  % vector tong cac vector fft
+        TongcacVector = 0;                  % vector tong cac vector fft
         for m=1:SoLuongKhung                               
             [frame] = Chiakhung(x, fs, DoDaiKhung, DoDichKhung, m);         
             [result] = FFT(frame, SoPhanTuVector_fft);                                    
-            Total = Total + result';           % tong cac vector fft
+            TongcacVector = TongcacVector + result';           % tong cac vector fft
         end 
-        Total = Total/SoLuongKhung;            % vector fft trung binh (1 nguyen am/1 nguoi noi)
-        Total = Total(1:length(Total)/2);      % lay 1 nua vector (vi la vector doi xung)
+        TongcacVector = TongcacVector/SoLuongKhung;            % vector fft trung binh (1 nguyen am/1 nguoi noi)
+        TongcacVector = TongcacVector(1:length(TongcacVector)/2);      % lay 1 nua vector (vi la vector doi xung)
         
         array = zeros(1,5);      % mang chua khoang cach giua VDT vua tinh voi VDT cua file huan luyen
         for m =1:5                                             
             vantay = VectorDacTrung(m,:);           % trich 1 vector dac trung cua 1 nguyen am 
-            array(m) = KhoangCachEuclidean(Total,vantay);           
+            array(m) = KhoangCachEuclidean(TongcacVector,vantay);           
         end
         [value,index] = min(array);          % nguyen am co khoang cach be nhat se la ket qua can tim
         
@@ -47,41 +47,19 @@ for i = 1 : length(TenFolder)
     disp('-------------------------------------------------- ');
 end
 
-disp('Ma tran nham lan :');
-disp('          a      e      i     o     u  ');
-disp('  ');
-for l=1:5
-    line = strcat(cell2mat(NguyenAm(l)),' ');
-    for p=1:5
-        line = strcat(line,{'     '}, num2str(MaTranNhamLan(l,p)));
-    end
-    disp(line);
-end
-disp('---------------------------------------------- ');
+DoChinhXacA = MaTranNhamLan(1, 1) / 21 * 100;
+DoChinhXacE = MaTranNhamLan(2, 2) / 21 * 100;
+DoChinhXacI = MaTranNhamLan(3, 3) / 21 * 100;
+DoChinhXacO = MaTranNhamLan(4, 4) / 21 * 100;
+DoChinhXacU = MaTranNhamLan(5, 5) / 21 * 100;
 
-DoChinhXacA=MaTranNhamLan(1,1)/21*100; 
-disp(strcat('Do chinh xac nguyen am /a/ :',num2str(DoChinhXacA),'%'));
-DoChinhXacE=MaTranNhamLan(2,2)/21*100;
-disp(strcat('Do chinh xac nguyen am /e/ :',num2str(DoChinhXacE),'%'));
-DoChinhXacI=MaTranNhamLan(3,3)/21*100;
-disp(strcat('Do chinh xac nguyen am /i/ :',num2str(DoChinhXacI),'%'));
-DoChinhXacO=MaTranNhamLan(4,4)/21*100;
-disp(strcat('Do chinh xac nguyen am /o/ :',num2str(DoChinhXacO),'%'));
-DoChinhXacU=MaTranNhamLan(5,5)/21*100;
-disp(strcat('Do chinh xac nguyen am /u/ :',num2str(DoChinhXacU),'%'));
+tong = (DoChinhXacA + DoChinhXacE + DoChinhXacI + DoChinhXacO + DoChinhXacU) / 5;
 
-Tong=0;
-for k=1:5
-    Tong=Tong+MaTranNhamLan(k,k);
-end
-Tong=(Tong/(21*5))*100;
-disp(strcat('Do chinh xac tong          :',num2str(Tong),'%'));
-%tableData = array2table(MaTranNhamLan, 'VariableNames', {'a', 'e', 'i', 'o', 'u'}, 'RowNames', {'a', 'e', 'i', 'o', 'u'});
-% Hi?n th? b?ng d? li?u
-%disp('Confusion Matrix:');
-%disp(tableData);
+MaTranNhamLan(1, 6) = DoChinhXacA;
+MaTranNhamLan(2, 6) = DoChinhXacE;
+MaTranNhamLan(3, 6) = DoChinhXacI;
+MaTranNhamLan(4, 6) = DoChinhXacO;
+MaTranNhamLan(5, 6) = DoChinhXacU;
+MaTranNhamLan(6, 6) = tong;
 figure;
-uitable('Data', MaTranNhamLan, 'ColumnName', {'a', 'e', 'i', 'o', 'u'}, 'RowName', {'a', 'e', 'i', 'o', 'u'}, 'Units', 'Normalized', 'Position', [0, 0, 1, 1]);
-
-
-
+uitable('Data', MaTranNhamLan, 'ColumnName', {'a', 'e', 'i', 'o', 'u', '?? chính xác (%)'}, 'RowName', {'a', 'e', 'i', 'o', 'u', '?? chính xác TB'}, 'Units', 'Normalized', 'Position', [0, 0, 1, 1]);
